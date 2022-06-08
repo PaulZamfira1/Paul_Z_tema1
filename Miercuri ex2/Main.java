d72c3deafd7847848c62e22b1e279c0d64151ae8
PaulZamfira1 <paul.zamfira@endava.com>
2022-06-08 13:49:16 +0300

    Update Main.java

--- Miercuri ex2/Main.java
+++ Miercuri ex2/Main.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int [] a = {2,2,2,4,5};
+        int [] a = {2,1,1,1,2};
         boolean b = false;
 
         for (int i = 0; i < a.length - 2; i++){
-                if(a[i] % 2 == 0 && a[i+1] % 2 == 0 && a[i+2] % 2 == 0) {
+                if((a[i] % 2 == 0 && a[i+1] % 2 == 0 && a[i+2] % 2 == 0) ||
+                        (a[i] % 2 == 1 && a[i+1] % 2 == 1 && a[i+2] % 2 == 1)) {
                     b = true;
 
-                }else if(a[i] % 2 == 1 && a[i+1] % 2 == 1 && a[i+2] % 2 == 1){
-                    b = true;
-                }else b = false;
+                }
         }
         System.out.println(b);
 
