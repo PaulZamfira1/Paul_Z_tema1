e7bdd9b8145be3f5b5e32042eac643a4cf12891c
PaulZamfira1 <paul.zamfira@endava.com>
2022-06-09 12:07:28 +0300

    Update Main.java

--- Miercuri ex2/Main.java
+++ Miercuri ex2/Main.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int [] a = {2,1,1,1,2};
+//        int [] a = {2,1,1,1,2};
         boolean b = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cate numere folositi?");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        System.out.println("Introduceti numerele folosite: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < a.length - 2; i++){
-                if((a[i] % 2 == 0 && a[i+1] % 2 == 0 && a[i+2] % 2 == 0) ||
-                        (a[i] % 2 == 1 && a[i+1] % 2 == 1 && a[i+2] % 2 == 1)) {
-                    b = true;
+        for (int i = 0; i < array.length - 2; i++){
+            if((array[i] % 2 == 0 && array[i+1] % 2 == 0 && array[i+2] % 2 == 0) ||
+                    (array[i] % 2 == 1 && array[i+1] % 2 == 1 && array[i+2] % 2 == 1)) {
+                b = true;
 
-                }
+            }
         }
         System.out.println(b);
 
